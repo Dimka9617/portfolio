e8b89e68773bb68b1e89df4141e47af229e3bb67
Dmytro <141151850+Dimka9617@users.noreply.github.com>
2024-05-04 01:55:23 +0300

    Add files via upload

--- guitar/js/script.js
+++ guitar/js/script.js
@@ -1,4 +1,5 @@
 "use strict"
+// NAVIGATION ON MENU-LINK ================================================================================
 document.addEventListener('click', docAct)
 function docAct(event){
 
@@ -15,13 +16,66 @@ function docAct(event){
 	}
 }
 }
+// ANIMATION ON VIEWPORT======================================================================================
+
+// function handleResize() {
+	
+// 	if (window.innerWidth < 768) {
+// 		 const options = {
+// 			  root: null,
+// 			  rootMargin: "0px 0px 0px 0px",
+// 			  threshold: 0.3,
+// 		 };
+
+// 		 const callBack = (entries, observer) => {
+// 			  entries.forEach((entry) => {
+// 					const targetElement = entry.target;
+// 					if (entry.isIntersecting) {
+// 						 targetElement.classList.add("move-left");
+// 					} else {
+// 						 targetElement.classList.remove("move-left");
+// 					}
+// 			  });
+// 		 };
+
+// 		 const observer = new IntersectionObserver(callBack, options);
+// 		 const target = document.querySelector(".brands__row-left");
+// 		 observer.observe(target);
+// 	}
+	
+// }
+// window.addEventListener("resize", handleResize);
+
+// ------------------------------------------------------
+// function handleResizeR() {
+// 	if(window.innerWidth < 768){
+// 		const options = {
+// 			root: null,
+// 			rootMargin: "0px 0px 0px 0px",
+// 			threshold: 0.3,
+// 	  };
+
+// 	  const callBackR = (entries, observer) => {
+// 			entries.forEach((entry) => {
+// 				 const targetElement = entry.target;
+// 				 if (entry.isIntersecting) {
+// 					  targetElement.classList.add("move-right");
+// 				 } else {
+// 					  targetElement.classList.remove("move-right");
+// 				 }
+// 			});
+// 	  };
+
+// 	  const observer = new IntersectionObserver(callBackR, options);
+// 	  const target = document.querySelector(".brands__row-right");
+// 	  observer.observe(target);
+// 	}
+// }
+
+// window.addEventListener("resize", handleResizeR);
 
 
-
-
-
-
-
+// SWIPER ================================================================================
 const customerSlider = document.querySelector('.customer__slider')
 if(customerSlider){
 
@@ -40,4 +94,14 @@ if(customerSlider){
  
  });
 }
-
+// BURGER================================================================================
+document.addEventListener('click', documentAction)
+function documentAction(e){
+	const targetElement = e.target
+	if(targetElement.closest(".icon-menu")){
+		document.documentElement.classList.toggle('menu-open')
+	}
+	else if(targetElement.closest('.menu__link')){
+		document.documentElement.classList.remove('menu-open')
+	}
+}
